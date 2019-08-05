ff0b613fd487436cbe79bd280e34f60e612b6f20
Yunfei Zhang <beethovenzhang@gmail.com>
2019-08-04 22:43:06 -0700

    learn variables in C

---
+++ Variables in C/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name [] = "John";
+    int age = 18;
+    printf("Hello world! My name is %s.\n", name);
+    printf("And I'm %d years old.\n", age);
+    return 0;
+}
